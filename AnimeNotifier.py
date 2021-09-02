e8c900f6cf4031acb781a2ec2b796b7340b1fd20
Mridul Roy <69635889+MridulRoy98@users.noreply.github.com>
2021-09-02 21:56:40 +0600

    Update AnimeNotifier.py

--- AnimeNotifier.py
+++ AnimeNotifier.py
@@ -6,8 +6,41 @@ import requests
 import sys
 sys.setrecursionlimit(10000)
 
+def getTimer(url):
+    """Scrapes the site for the timer"""
+
+    html = requests.get('http://9anime.to'+url)
+    html.raise_for_status()
+    soup = bs4.BeautifulSoup(html.text, 'html.parser')
+    # print(soup.prettify())
+    newSoup = soup.find('span', {'class': 'timer'})
+    timer = newSoup.text
+    commaSplitList = timer.split(',')
+
+    hours = []
+    days = []
+    minutes = []
+    days = commaSplitList[0]
+    hours = commaSplitList[1]
+    minutes = commaSplitList[2]
+
+    daysSplit = int(days.split(' ')[0])
+    hoursSplit = int(hours.split(' ')[1])
+    minutesSplit = int(minutes.split(' ')[1])
+
+    print(daysSplit, "days ", hoursSplit, "hours ", minutesSplit, "minutes ")
+
+    # numberOfHours = int(hours[0])
+    # numberOfMinutes = int(minutes[0])
+    # print(numberOfDays)
+    # print(numberOfHours)
+    # for i in commaSplit:
+    #     spaceSplit = commaSplit.split(' ')
+    #     print(spaceSplit)
 
 def merge_pickles(pkl1, pkl2, mergedpkl):
+    """Merges two pickle files into one"""
+
     with open(pkl1, 'rb') as file:
         dic1 = pickle.load(file)
         file.close()
@@ -21,6 +54,8 @@ def merge_pickles(pkl1, pkl2, mergedpkl):
 
 
 def read_pickle(pkl):
+    """Reads the merged pickle file to look for desired anime"""
+
     with open(pkl, 'rb') as file:
         dict = pickle.load(file)
         file.close()
@@ -28,12 +63,16 @@ def read_pickle(pkl):
 
 
 def write_pickle(data, pkl):
+    """Writes the data into individual pickle file"""
+
     with open(pkl, 'wb') as file:
         pickle.dump(data, file)
         file.close()
 
 
 def get_html(url, pageNumber):
+    """Downloads the html and returns the text within"""
+
     htmlPage = ""
     flag = True
     while flag:
@@ -52,7 +91,8 @@ def get_html(url, pageNumber):
 
 
 def scrape_pages(start, end, pklfile):
-    # Looping through Page numbers in the website
+    """Scrapes the site for list of animes and their corresponding links"""
+
     pageNumber = start
     linkList = []
     nameList = []
@@ -61,6 +101,7 @@ def scrape_pages(start, end, pklfile):
     while pageNumber <= end:  # Max Pages = 424
         print(f"Scraping page number: {pageNumber}")
         url = 'https://9anime.to/az-list?page=' + str(pageNumber)
+
         # Converting to Beautiful soup
         soup = bs4.BeautifulSoup(get_html(url, pageNumber), 'html.parser')
 
@@ -102,7 +143,8 @@ if __name__ == "__main__":
     desiredAnime = input("Which anime do you want to follow: ")
     for key in animeDictionary.keys():
         if desiredAnime.lower() in key.lower():
-            print(animeDictionary[desiredAnime])
+            getTimer(animeDictionary[desiredAnime])
+            # print(animeDictionary[desiredAnime])
             flag = False
             break
         else:
